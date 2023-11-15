68b6fdfed84424e18209d71e1cca900f18e832ac
SoyQuillo <santiagoquintero190305@gmail.com>
2023-11-15 08:48:29 -0500

    multi

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import { ref } from 'vue'
+
+let numberEnter = ref(0);
+
+let aux = ref(0);
+
+function doClick(){
+
+aux.value = numberEnter.value
+
+}
+
+</script>
+
+<template>
+
+<div>
+ <input v-model="numberEnter"  type="number">
+ <button @click="doClick">PRESIONAR</button>
+ <h1>{{ numberEnter }}</h1>
+
+ <h1 v-for="i in 10" :key="i" >
+{{ aux * i }}
+</h1>
+</div>
+  
+</template>
+
+<style scoped>
+
+</style>
